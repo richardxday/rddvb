e3b7be26a71fc34502014b4bea577d3bc4401e07
Richard Day <r.day80@gmail.com>
2015-01-14 21:21:22 +0000

    Don't return error code based on programme list size by default, only do that if --return-count is specified.

--- bin/checksky.sh
+++ bin/checksky.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd ~/programmes-of-interest
-if dvb -r /etc/dvb/data/skylistings.dat --find-with-file patterns.txt -w foundprogrammes.dat ; then
+if ! dvb -r /etc/dvb/data/skylistings.dat --find-with-file patterns.txt -w foundprogrammes.dat --return-count ; then
   echo "Summary:" >newlist.txt
   dvb -r foundprogrammes.dat -v --list-to-file newlist.txt >/dev/null
   echo "" >>newlist.txt
@@ -8,8 +8,10 @@ if dvb -r /etc/dvb/data/skylistings.dat --find-with-file patterns.txt -w foundpr
   dvb -r foundprogrammes.dat -v10 --list-to-file newlist.txt >/dev/null
   if ! diff newlist.txt list.txt >/dev/null ; then
     cp newlist.txt list.txt
-    echo "Sending email"
+    echo "List changed: sending email"
     mail -s "DVB Programmes of Interest" r.day80@gmail.com <list.txt
+  else
+   echo "List hasn't changed"
   fi
   rm newlist.txt
 fi
