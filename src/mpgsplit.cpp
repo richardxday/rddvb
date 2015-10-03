c5decc19a033be72aa505cd4eab5dff752d30cf8
Richard Day <r.day80@gmail.com>
2015-10-03 16:36:28 +0100

    Use simple concatenation of split files.  Process subtitle index file so that subtitles work with split file

--- src/mpgsplit.cpp
+++ src/mpgsplit.cpp
@@ -9,20 +9,34 @@
 #include <rdlib/StdFile.h>
 #include <rdlib/Recurse.h>
 
-double CalcTime(const char *str)
+uint64_t CalcTime(const char *str)
 {
 	uint_t hrs = 0, mins = 0, secs = 0, ms = 0;
-	double t = 0.0;
+	uint64_t t = 0;
 	
-	if (sscanf(str, "%u:%u:%u.%u", &hrs, &mins, &secs, &ms) >= 3) {
-		uint64_t _t = (uint64_t)(hrs * 3600 + mins * 60 + secs) * 1000UL + ms;
-
-		t = (double)_t / 1000.0;
-
-		//printf("'%s' -> %0.3lf\n", str, t);
+	if (sscanf(str, "%u:%u:%u:%u", &hrs, &mins, &secs, &ms) >= 4) {
+		t = (uint64_t)(hrs * 3600 + mins * 60 + secs) * 1000UL + ms;
+	}
+	else if (sscanf(str, "%u:%u:%u.%u", &hrs, &mins, &secs, &ms) >= 4) {
+		t = (uint64_t)(hrs * 3600 + mins * 60 + secs) * 1000UL + ms;
 	}
 
 	return t;
+}
+
+AString GenTime(uint64_t t, const char *format = "%02u:%02u:%02u.%03u")
+{
+	AString  res;
+	uint32_t sec = (uint32_t)(t / 1000);
+	uint_t   ms  = (uint_t)(t % 1000);
+
+	res.printf(format,
+			   (uint_t)(sec / 3600),
+			   (uint_t)((sec / 60) % 60),
+			   (uint_t)(sec % 60),
+			   ms);
+
+	return res;
 }
 
 AString GetParentheses(const AString& line, int p = 0)
@@ -37,14 +51,23 @@ AString GetParentheses(const AString& line, int p = 0)
 	return "";
 }
 
+void CopyFile(AStdData& fp1, AStdData& fp2)
+{
+	static uint8_t buffer[65536];
+	slong_t l;
+
+	while ((l = fp1.readbytes(buffer, sizeof(buffer))) > 0) fp2.writebytes(buffer, l);
+}
+
 typedef struct {
 	AString aspect;
-	double  start, length;
+	uint64_t  start, length;
 } SPLIT;
 
 int main(int argc, char *argv[])
 {
 	AStdFile fp;
+	bool cleanup = true;
 	
 	if (argc < 3) {
 		fprintf(stderr, "Usage: mpgsplit <src-file> <dst-file> [<aspect>...]\n");
@@ -57,9 +80,14 @@ int main(int argc, char *argv[])
 	AString remuxsrc = basename + "_Remuxed.mpg";
 	AString logfile  = basename + "_log.txt";
 	std::map<AString,bool> desired_aspects;
-	int     rc = 0, arg, stage = 1;
+	int     rc = 0, arg;
 
-	for (arg = 3; arg < argc; arg++) desired_aspects[AString(argv[arg])] = true;
+	for (arg = 3; arg < argc; arg++) {
+		if (argv[arg][0] == '-') {
+			if (stricmp(argv[arg], "-keep") == 0) cleanup = false;
+		}
+		else desired_aspects[AString(argv[arg])] = true;
+	}
 
 	if (!AStdFile::exists(AString("%s.m2v").Arg(basename)) ||
 		!AStdFile::exists(AString("%s.mp2").Arg(basename)) ||
@@ -72,17 +100,15 @@ int main(int argc, char *argv[])
 		printf("Executing: '%s'\n", cmd.str());
 		if (system(cmd) != 0) {
 			printf("**** Failed\n");
-			rc = -stage;
+			rc = -__LINE__;
 		}
 		remove(basename + ".sup");
 		remove(basename + ".sup.IFO");
 		printf("--------------------------------------------------------------------------------\n");
 	}
 
-	stage++;
-	
 	std::vector<SPLIT> splits;
-	std::map<AString,double> lengths;
+	std::map<AString,uint64_t> lengths;
 	AString bestaspect;
 	
 	if (!rc) {
@@ -90,11 +116,11 @@ int main(int argc, char *argv[])
 			static const AString formatmarker = "new format in next leading sequenceheader detected";
 			static const AString videomarker  = "video basics";
 			static const AString lengthmarker = "video length:";
-			AString line;
-			AString aspect = "16:9";
-			double  t1     = 0.0;
-			double  t2     = 0.0;
-			double  totallen = 0.0;
+			AString  line;
+			AString  aspect = "16:9";
+			uint64_t t1     = 0;
+			uint64_t t2     = 0;
+			uint64_t totallen = 0;
 
 			printf("--------------------------------------------------------------------------------\n");
 			printf("Analysing logfile:\n\n");
@@ -112,7 +138,7 @@ int main(int argc, char *argv[])
 					if (t2 > t1) {
 						SPLIT split = {aspect, t1, t2 - t1};
 
-						printf("%-6s @ %0.3lf for %0.3lfs\n", split.aspect.str(), split.start, split.length);
+						printf("%-6s @ %s for %s\n", split.aspect.str(), GenTime(split.start).str(), GenTime(split.length).str());
 
 						splits.push_back(split);
 
@@ -127,29 +153,29 @@ int main(int argc, char *argv[])
 					if ((p = line.Pos(" @ ", p)) >= 0) {
 						p += 3;
 						totallen = CalcTime(line.str() + p);
-						printf("Total length %0.3lfs\n", totallen);
+						printf("Total length %s\n", GenTime(totallen).str());
 					}
 				}
 			}
 
 			{
-				SPLIT split = {aspect, t1, 0.0};
+				SPLIT split = {aspect, t1, 0};
 				splits.push_back(split);
 
-				if (totallen > 0.0) lengths[aspect] += totallen - t1;
+				if (totallen > 0) lengths[aspect] += totallen - t1;
 				if (bestaspect.Empty() || (lengths[aspect] > lengths[bestaspect])) bestaspect = aspect;
 			}
 
 			fp.close();
 
 			if (bestaspect.Valid()) {
-				printf("Best aspect is %s with %0.3lfs\n", bestaspect.str(), lengths[bestaspect]);
+				printf("Best aspect is %s with %s\n", bestaspect.str(), GenTime(lengths[bestaspect]).str());
 				desired_aspects[bestaspect] = true;
 			}
 		}
 		else {
 			fprintf(stderr, "Failed to open log file '%s'\n", logfile.str());
-			rc = -stage;
+			rc = -__LINE__;
 		}
 	}
 
@@ -166,7 +192,7 @@ int main(int argc, char *argv[])
 			if (system(cmd) != 0) {
 				printf("**** Failed\n");
 				printf("--------------------------------------------------------------------------------\n");
-				rc = -stage;
+				rc = -__LINE__;
 			}
 		}
 		else {
@@ -181,12 +207,10 @@ int main(int argc, char *argv[])
 				printf("Executing: '%s'\n", cmd.str());
 				if (system(cmd) != 0) {
 					printf("**** Failed\n");
-					rc = -stage;
+					rc = -__LINE__;
 				}
 				printf("--------------------------------------------------------------------------------\n");
 			}
-
-			stage++;
 
 			std::map<AString,bool>::iterator it;
 			uint_t outputindex = 0;
@@ -205,8 +229,8 @@ int main(int argc, char *argv[])
 						outfile.printf("%s-%s-%u.mpg", basename.str(), aspect.SearchAndReplace(":", "_").str(), i);
 
 						if (!AStdFile::exists(outfile)) {
-							cmd.printf("avconv -fflags +genpts -i \"%s\" -ss %0.3lf", remuxsrc.str(), split.start);
-							if (split.length > 0.0) cmd.printf(" -t %0.3lf", split.length);
+							cmd.printf("avconv -fflags +genpts -i \"%s\" -ss %s", remuxsrc.str(), GenTime(split.start).str());
+							if (split.length > 0) cmd.printf(" -t %s", GenTime(split.length).str());
 							cmd.printf(" -acodec copy -vcodec copy -v warning -y -f mpegts \"%s\"", outfile.str());
 				
 							printf("--------------------------------------------------------------------------------\n");
@@ -214,68 +238,145 @@ int main(int argc, char *argv[])
 							if (system(cmd) != 0) {
 								printf("**** Failed\n");
 								printf("--------------------------------------------------------------------------------\n");
-								rc = -stage;
+								rc = -__LINE__;
 								break;
 							}
 							printf("--------------------------------------------------------------------------------\n");
 						}
 
-						stage++;
-	
 						files.push_back(outfile);
 					}
 				}
 
 				{
-					AString cmd;
-					AString inputfiles;
+					AString outputfile;
+					
+					outputfile.printf("%s%s.%s", dst.Prefix().str(), outputindex ? AString(".%u").Arg(outputindex).str() : "", dst.Suffix().str());
+					
+					if (!AStdFile::exists(outputfile)) {
+						AStdFile ofp;
+						AString  concatfile;
 
-					for (i = 0; i < files.size(); i++) {
-						if (inputfiles.Empty()) inputfiles.printf("concat:%s", files[i].str());
-						else					inputfiles.printf("|%s",       files[i].str());
+						concatfile = outputfile.Prefix() + "_Concat.mpg";
+
+						if (ofp.open(concatfile, "wb")) {
+							for (i = 0; i < files.size(); i++) {
+								AStdFile ifp;
+
+								if (ifp.open(files[i], "rb")) {
+									printf("Adding '%s'...\n", files[i].str());
+									CopyFile(ifp, ofp);
+									ifp.close();
+								}
+								else {
+									printf("Failed to open '%s' for reading\n", files[i].str());
+									rc = -__LINE__;
+									break;
+								}
+							}
+
+							ofp.close();
+						}
+						else {
+							printf("Failed to open '%s' for writing\n", concatfile.str());
+							rc = -__LINE__;
+						}
+
+						if (!rc) {
+							AString cmd;
+
+							cmd.printf("avconv -i \"%s\" -aspect %s -acodec mp3 -vcodec copy -filter:v yadif -v warning -y \"%s\"", concatfile.str(), aspect.str(), outputfile.str());
+						
+							printf("--------------------------------------------------------------------------------\n");
+							printf("Executing: '%s'\n", cmd.str());
+							if (system(cmd) != 0) {
+								printf("**** Failed\n");
+								printf("--------------------------------------------------------------------------------\n");
+								rc = -__LINE__;
+								break;
+							}
+						}
+
+						if (cleanup) remove(concatfile);
 					}
 					
-					cmd.printf("avconv -i \"%s\" -aspect %s -acodec mp3 -vcodec copy -filter:v yadif -v warning -y \"%s%s.%s\"", inputfiles.str(), aspect.str(), dst.Prefix().str(), outputindex ? AString(".%u").Arg(outputindex).str() : "", dst.Suffix().str());
-
 					printf("--------------------------------------------------------------------------------\n");
-					printf("Executing: '%s'\n", cmd.str());
-					if (system(cmd) != 0) {
-						printf("**** Failed\n");
-						printf("--------------------------------------------------------------------------------\n");
-						rc = -stage;
-						break;
-					}
-					else {
-						printf("--------------------------------------------------------------------------------\n");
 
+					if (cleanup) {
 						for (i = 0; i < files.size(); i++) {
 							remove(files[i]);
 						}
 					}
+					
+					FILE_INFO info;
+					if (GetFileInfo(dst.PathPart().CatPath("subs"), &info)) {
+						AString srcsubfile = src.Prefix() + ".sup.idx";
+						AString dstsubfile = outputfile.PathPart().CatPath("subs", outputfile.FilePart().Prefix() + ".sup.idx");
+						AStdFile fp1, fp2;
+						
+						if (fp1.open(srcsubfile)) {
+							if (fp2.open(dstsubfile, "w")) {
+								AString line;
 
-					stage++;
+								printf("Converting '%s' to '%s'\n", srcsubfile.str(), dstsubfile.str());
+								
+								while (line.ReadLn(fp1) >= 0) {
+									if (line.Pos("timestamp:") == 0) {
+										uint64_t t = CalcTime(line.str() + 10);
+										uint64_t sub = 0;
+
+										for (i = 0; i < splits.size(); i++) {
+											const SPLIT& split = splits[i];
+											
+											if (split.aspect == aspect) {
+												if ((t >= split.start) && (t < (split.start + split.length))) {
+													t -= sub;
+													
+													fp2.printf("timestamp: %s, filepos: %s\n",
+															   GenTime(t, "%02u:%02u:%02u:%03u").str(),
+															   line.Mid(34).str());
+													break;
+												}
+											}
+											else sub += split.length;
+										}
+									}
+									else fp2.printf("%s\n", line.str());
+								}
+
+								fp2.close();
+							}
+							else printf("Failed to open sub file '%s' for writing\n", dstsubfile.str());
+
+							fp1.close();
+						}
+						else printf("Failed to open sub file '%s' for reading\n", srcsubfile.str());
+
+						srcsubfile = src.Prefix() + ".sup.sub";
+						dstsubfile = outputfile.PathPart().CatPath("subs", outputfile.FilePart().Prefix() + ".sup.sub");
+						if (fp1.open(srcsubfile, "rb")) {
+							if (fp2.open(dstsubfile, "wb")) {
+								printf("Copying '%s' to '%s'\n", srcsubfile.str(), dstsubfile.str());
+								
+								CopyFile(fp1, fp2);
+								
+								fp2.close();
+							}
+							else printf("Failed to open sub file '%s' for writing\n", dstsubfile.str());
+
+							fp1.close();
+						}
+						else printf("Failed to open sub file '%s' for reading\n", srcsubfile.str());
+					}
 				}
 			}
 		}
 	}
 
-	if (!rc) {
-		FILE_INFO info;
-	  
-		if (GetFileInfo(dst.PathPart().CatPath("subs"), &info)) {
-			AString cmd;
-
-			cmd.printf("mv \"%s.sup\".* \"%s\"", basename.str(), dst.PathPart().CatPath("subs").str());
-			if (system(cmd) != 0) printf("Failed to move .sup files\n");
-		}
-		else {
-			remove(basename + ".sup.idx");
-			remove(basename + ".sup.sub");
-		}
-		
+	if (!rc && cleanup) {
 		remove(basename + ".m2v");
 		remove(basename + ".mp2");
-		remove(basename + "_Remuxed.mpg");
+		remove(remuxsrc);
 		remove(logfile);
 	}
 	
