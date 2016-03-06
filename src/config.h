e43c938dd99797da81e6aa9a80114f1a1d9728aa
Richard Day <r.day80@gmail.com>
2016-03-06 10:31:52 +0000

    Fix incorrect option string.

--- src/config.h
+++ src/config.h
@@ -79,7 +79,7 @@ public:
 	double  GetLowSpaceWarningLimit()		 const {return (double)GetConfigItem("lowdisklimit", "10.0");}
 	bool    CommitScheduling()				 const {return ((uint_t)GetConfigItem("commitscheduling", "0") != 0);}
 
-	bool    IsRecordingSlave()				 const {return ((uint_t)GetConfigItem("convertvideos", "0"));}
+	bool    IsRecordingSlave()				 const {return ((uint_t)GetConfigItem("isrecordingslave", "0"));}
 	bool    ConvertVideos()					 const {return ((uint_t)GetConfigItem("convertvideos", AString("%").Arg(!IsRecordingSlave())));}
 	bool    EnableCombined()				 const {return ((uint_t)GetConfigItem("enablecombined", AString("%").Arg(!IsRecordingSlave())));}
 
