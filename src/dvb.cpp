3f6992512b0ea75a1def13b521572528ce51a854
Richard Day <r.day80@gmail.com>
2016-03-12 15:12:41 +0000

    Set post-processing and programme to processing list whilst converting video

--- src/dvb.cpp
+++ src/dvb.cpp
@@ -143,7 +143,7 @@ int main(int argc, char *argv[])
 		printf("\t--show-encoding-args\t\tShow encoding arguments for programmes in current list\n");
 		printf("\t--show-file-format\t\tShow file format of encoded programme\n");
 		printf("\t--use-converted-filename\tChange filename to that of converted file, without actually converting file\n");
-		printf("\t--regenerate-file\t\tChange filename of current list of programmes (dependant on converted state), renaming files (in original directory OR current directory)\n");
+		printf("\t--regenerate-filename\t\tChange filename of current list of programmes (dependant on converted state), renaming files (in original directory OR current directory)\n");
 		printf("\t--delete-files\t\t\tDelete encoded files from programmes in current list\n");
 		printf("\t--record-success\t\tRun recordsuccess command on programmes in current list\n");
 		printf("\t--change-user <patterns> <newuser>\n\t\t\t\t\tChange user of programmes matching <patterns> to <newuser>\n");
@@ -1114,8 +1114,10 @@ int main(int argc, char *argv[])
 
 				for (j = 0; (j < proglist.Count()) && !HasQuit(); j++) {
 					ADVBProg& prog = proglist.GetProgWritable(j);
-					AString oldfilename = prog.GetFilename();
-					AString newfilename = oldfilename.PathPart().CatPath(prog.GenerateFilename(prog.IsConverted()).FilePart());
+					AString oldfilename  = prog.GetFilename();
+					AString newfilename  = oldfilename.PathPart().CatPath(prog.GenerateFilename(prog.IsConverted()).FilePart());
+					AString oldfilename1 = oldfilename.FilePart();
+					AString newfilename1 = newfilename.FilePart();
 
 					if (newfilename != oldfilename) {
 						prog.SetFilename(newfilename);
@@ -1126,14 +1128,13 @@ int main(int argc, char *argv[])
 							}
 							else fprintf(stderr, "Failed to rename '%s' to '%s'\n", oldfilename.str(), newfilename.str());
 						}
-						else if (AStdFile::exists(oldfilename.FilePart())) {
-							AString oldfilename1 = oldfilename.FilePart();
-							AString newfilename1 = newfilename.FilePart();
+						else if (AStdFile::exists(oldfilename1)) {
 							if (rename(oldfilename1, newfilename1) == 0) {
 								printf("Renamed '%s' to '%s'\n", oldfilename1.str(), newfilename1.str());
 							}
 							else fprintf(stderr, "Failed to rename '%s' to '%s'\n", oldfilename1.str(), newfilename1.str());
 						}
+						else printf("Neither '%s' nor '%s' exists\n", oldfilename.str(), oldfilename1.str());
 					}
 				}
 			}
