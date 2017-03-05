134b200c408d044289c9645f5eca91629d65206f
Richard Day <r.day80@gmail.com>
2017-03-05 13:37:10 +0000

    Back up DVB config as well

--- scripts/backup-dvb.sh
+++ scripts/backup-dvb.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 DATA="/var/dvb"
+CONFIG="/etc/dvb"
 DEST="/home/richard/Dropbox/Personal/dvb"
 COUNT=14
-
-cd "$DATA"
 
 INDEX=$(((`date +%s`/3600/24)%$COUNT))
 FILE="$DEST/dvbdata.$INDEX.7z"
 if [ -f "$FILE" ] ; then
   rm "$FILE"
 fi
-7z a "$FILE" recorded.dat episodes.txt
-
+7z a "$FILE" $DATA/recorded.dat $DATA/episodes.txt $CONFIG/*
