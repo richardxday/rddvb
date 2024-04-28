9c382260f443d95b5321c57bc87c5bc9a31865d2
Richard Day <r.day80@gmail.com>
2024-04-28 21:40:16 +0100

    Add extra information when programme has already been converted.

--- src/proglist.cpp
+++ src/proglist.cpp
@@ -3265,7 +3265,7 @@ bool ADVBProgList::GetAndConvertRecordings()
             config.printf("File %u/%u - '%s' is already being processed", i + 1, convertlist.Count(), prog.GetQuickDescription().str());
         }
         else if (((pprog = reclist.FindUUID(prog)) != NULL) && pprog->IsConverted() && !pprog->HasFailed()) {
-            config.printf("File %u/%u - '%s' has already successfully been converted", i + 1, convertlist.Count(), prog.GetQuickDescription().str());
+            config.printf("File %u/%u - '%s' has already successfully been converted: %s", i + 1, convertlist.Count(), prog.GetQuickDescription().str(), pprog->GetQuickDescription().str());
         }
         else {
             config.printf("Converting file %u/%u - '%s':", i + 1, convertlist.Count(), prog.GetQuickDescription().str());
